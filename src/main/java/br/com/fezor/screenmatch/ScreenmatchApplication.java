50e313190820aa5d054990bc35b55d876f5fc540
felipezzz1 <felipezorzo78@gmail.com>
2024-10-14 21:16:04 -0300

    add user input

--- src/main/java/br/com/fezor/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/fezor/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,9 @@
 package br.com.fezor.screenmatch;
 
-import br.com.fezor.screenmatch.models.EpisodeData;
-import br.com.fezor.screenmatch.models.SeasonData;
-import br.com.fezor.screenmatch.models.SeriesData;
-import br.com.fezor.screenmatch.services.DataConvert;
-import br.com.fezor.screenmatch.services.UseAPI;
-import io.github.cdimascio.dotenv.Dotenv;
+import br.com.fezor.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,28 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Dotenv dotenv = Dotenv.load();
-		String apiKey = dotenv.get("API_KEY");
-
-		var useAPI = new UseAPI();
-		var json = useAPI.getData("https://www.omdbapi.com/?t=prison+break&apikey=" + apiKey);
-
-		System.out.println(json);
-		DataConvert converter = new DataConvert();
-
-		SeriesData data = converter.getData(json, SeriesData.class);
-		System.out.println(data);
-
-		json = useAPI.getData("https://www.omdbapi.com/?t=prison+break&season=1&episode=1&apikey=" + apiKey);
-		EpisodeData episodeData = converter.getData(json, EpisodeData.class);
-		System.out.println(episodeData);
-
-		List<SeasonData> seasons = new ArrayList<>();
-		for (int i = 0; i < data.totalSeasons(); i++) {
-			json = useAPI.getData("https://www.omdbapi.com/?t=prison+break&season=" + i + "&apikey=" + apiKey);
-			SeasonData seasonData = converter.getData(json, SeasonData.class);
-			seasons.add(seasonData);
-		}
-		seasons.forEach(System.out::println);
+		Main main = new Main();
+		main.showMenu();
 	}
 }
---
+++ src/main/java/br/com/fezor/screenmatch/main/Main.java
@@ -0,0 +1,45 @@
+package br.com.fezor.screenmatch.main;
+
+import br.com.fezor.screenmatch.models.SeasonData;
+import br.com.fezor.screenmatch.models.SeriesData;
+import br.com.fezor.screenmatch.services.DataConvert;
+import br.com.fezor.screenmatch.services.UseAPI;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private Scanner read = new Scanner(System.in);
+    private final String ADDRESS = "https://www.omdbapi.com/?t=";
+    private UseAPI useApi = new UseAPI();
+    private DataConvert dataConvert = new DataConvert();
+
+    Dotenv dotenv = Dotenv.load();
+    String apiKey = dotenv.get("API_KEY");
+
+    public void showMenu(){
+        System.out.println("Please type the serie you want to search: ");
+        var serieName = read.nextLine();
+
+
+        var json = useApi.getData(ADDRESS +
+                serieName.replace(" ","+") +
+                "&apikey=" + apiKey);
+
+        SeriesData data = dataConvert.getData(json, SeriesData.class);
+        System.out.println(data);
+
+        List<SeasonData> seasons = new ArrayList<>();
+		for (int i = 1; i <= data.totalSeasons(); i++) {
+			json = useApi.getData(ADDRESS +
+                    serieName.replace(" ", "+") +
+                    "&season=" + i +
+                    "&apikey=" + apiKey);
+			SeasonData seasonData = dataConvert.getData(json, SeasonData.class);
+			seasons.add(seasonData);
+		}
+		seasons.forEach(System.out::println);
+    }
+}
