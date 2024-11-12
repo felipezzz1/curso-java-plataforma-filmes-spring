606651a5358612766c33a2b20e52d2f2c7213aeb
felipezzz1 <felipezorzo78@gmail.com>
2024-11-12 14:45:08 -0300

    modelling category and series

---
+++ src/main/java/br/com/fezor/screenmatch/models/Series.java
@@ -0,0 +1,25 @@
+package br.com.fezor.screenmatch.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.util.OptionalDouble;
+
+public class Series {
+    private String title;
+    private Integer totalSeasons;
+    private Double rating;
+    private Category genre;
+    private String actors;
+    private String poster;
+    private String plot;
+
+    public Series(SeriesData seriesData){
+        this.title = seriesData.title();
+        this.totalSeasons = seriesData.totalSeasons();
+        this.rating = OptionalDouble.of(Double.valueOf(seriesData.rating())).orElse(0);
+        this.genre = Category.fromString(seriesData.genre().split(",")[0].trim());
+        this.actors = seriesData.actors();
+        this.poster = seriesData.poster();
+        this.plot = seriesData.plot();
+    }
+}
